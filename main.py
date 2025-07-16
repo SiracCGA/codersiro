fec1a371b5458f8454054bb803e121f6658e1259
SiracCGA <119678748+SiracCGA@users.noreply.github.com>
2025-07-16 16:46:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-while True:
-  print("Hello")
+# import flast module
+from flask import Flask
+
+# instance of flask application
+app = Flask(__name__)
+
+# home route that returns below text when root url is accessed
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':  
+   app.run()  
